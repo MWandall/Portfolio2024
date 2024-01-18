26503817b540712e3100b7345c9a7a2ed255bb94
MWandall <144394104+MWandall@users.noreply.github.com>
2024-01-18 16:17:04 -0600

    added darkmode and additional styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import { IoIosCloudyNight } from "react-icons/io";
-import { IoLogoGithub, IoLogoLinkedin, IoLogoDiscord, IoCloudDownload  } from "react-icons/io5";
+import { IoLogoGithub, IoLogoLinkedin, IoLogoDiscord, IoCloudDownload, IoCodeWorking  } from "react-icons/io5";
 import { IoDesktopOutline } from "react-icons/io5";
+import { useState } from "react";
+
 
 
 const projects = [
   {
     name: "Monster Mayhem",
+    text: "A full stack game concept.",
     image: "/MMscreenshot.png",
     githubLink: "https://github.com/MWandall/project3",
     liveDemoLink: "https://calm-meadow-66160-5c84b96ef414.herokuapp.com/",
   },
   {
     name: "GameU",
+    text: "Full stack game review/social media.",
     image: "/GameU.png",
     githubLink: "https://github.com/MWandall/gameU",
     liveDemoLink: "https://limitless-mountain-58094-a782c55b0be7.herokuapp.com/",
   },
   {
     name: "GigGuide",
+    text: "Find music events in your area for a spontaneous outing.",
     image: "/GigGuide.png",
     githubLink: "https://github.com/MWandall/concert-finder",
     liveDemoLink: "https://kevinsmithseven.github.io/concert-finder/",
@@ -25,39 +30,49 @@ const projects = [
 
   {
     name: "JATE",
+    text: "A downloadable progressive web app text editor.",
     image: "/JATE.png",
     githubLink: "https://github.com/MWandall/pwaTextEditor",
     liveDemoLink: "https://quiet-reaches-59584-957246460e74.herokuapp.com/",
   },
   {
     name: "E-Commerce Backend",
+    text: "SQL back end for a shop.",
     image: "/E-commerceBackend.png",
     githubLink: "https://github.com/MWandall/e-commerceBackEnd",
     liveDemoLink: "https://drive.google.com/file/d/1Jmo46EbtmVVWI_YGmUcayLBgoncXWbpu/view",
   },
   {
     name: "Social Network API Backend",
+    text: "NoSQL(MongoDB) backend for a social media site.",
     image: "/SocialNetworkApi.png",
     githubLink: "https://github.com/MWandall/socialNetworkAPI",
     liveDemoLink: "https://drive.google.com/file/d/19Eg94G54tOGbDNm4-JCB45SHi4LEeCm9/view",
   },
 ];
 
+
 function App() {
-  
+  const [darkMode, setDarkMode] = useState(false)
 
   return (
     <>
-    <main className="bg-white px-10">
+    <div className={darkMode ? "dark" : ""}>
+    <main className="bg-white px-10 dark:bg-slate-900 dark:text-white">
       <section className="  min-h-screen"> 
-      <nav className="py-10 mb-12 flex justify-end border-b-2 border-teal-500">
-        {/* <h1 className="text-xl"> ~~~ </h1> */}
+      
+      <nav className="py-10 mb-12 flex justify-between border-b-2 border-teal-500">
+        <div className="flex flex-col gap-7 text-4xl">
+        <a href="https://github.com/MWandall" target='_blank' rel="noreferrer"><IoLogoGithub className='  cursor-pointer hover:scale-125 transition-all ease-in-out'/></a>
+        <a href="https://www.linkedin.com/in/mitchell-wandall-1501222a5/" target='_blank' rel="noreferrer"><IoLogoLinkedin className='  cursor-pointer hover:scale-125 transition-all ease-in-out'/></a>
+        
+        </div>
         <ul className="flex items-center px-1 ">
 
           {/* <li className="pr-5 text-sm">(∩ ͡° ͜ʖ ͡°)⊃━☆ﾟ. *</li> */}
-          <li className="cursor-pointer hover:scale-125 transition-all ease-in-out shadow-lg p-3 rounded-xl my-2 flex flex-row flex-wrap items-center h-[5rem] bg-gradient-to-r  from-emerald-500 bg-cyan-500 text-gray-900 ">
+          <li onClick={()=> setDarkMode(!darkMode)} className="cursor-pointer hover:scale-125 transition-all ease-in-out shadow-lg p-3 rounded-xl my-2 flex flex-row flex-wrap items-center h-[5rem] bg-gradient-to-r  from-emerald-500 bg-cyan-500 text-gray-900 ">
           <p className=" text-sm">(∩ ͡° ͜ʖ ͡°)⊃━☆ﾟ. *</p>
-            <IoIosCloudyNight className="cursor-pointer text-4xl hover:scale-125 transition-all ease-in-out ml-4" />
+            <IoIosCloudyNight  className="cursor-pointer text-4xl hover:scale-125 transition-all ease-in-out ml-4" />
           
           </li>
 
@@ -77,27 +92,27 @@ function App() {
       </nav>
       
       <div className="text-center">
-        <h2 className="text-4xl py-2 text-teal-600 font-bold ">Mitchell Wandall</h2>
-        <h3 className="text-xl font-semibold">Full Stack Dev</h3>
-        <p className="text-lg py-5 leading-8">Freelance Developer for all of your programming and designing needs. Let's collaborate to bring your ideas to life with precision and creativity!</p>
+        <h2 className="text-4xl py-2 text-teal-600 font-bold md:text-5xl">Mitchell Wandall</h2>
+        <h3 className="text-xl font-semibold md:text-2xl">Full Stack Developer</h3>
+        <p className="text-lg py-5 leading-8 max-w-xl mx-auto md:text-xl">Freelance Developer for all of your programming and designing needs. Let's collaborate to bring your ideas to life with precision and creativity!</p>
       </div>
       <article className="flex justify-center mt-10">
-      <div className="h-80 w-80 rounded-full bg-gradient-to-b from-emerald-500 flex justify-center">
+      <div className="h-80 w-80 rounded-full bg-gradient-to-b from-emerald-500 flex justify-center md:h-96 md:w-96">
         <img className=" rounded-full " src="/Mitchell1.png" alt="picture of mitchell" />
       </div>
       </article>
 
 
-      <div className="flex flex-col items-center pb-[90px]">
+      <div className="flex flex-col items-center pb-[90px] ">
       {/* <h1 className="mb-5 shadow-lg p-10 rounded-xl my-2 flex flex-col justify-between w-[50rem] text-center">
         Portfolio Page
       </h1> */}
 
-      <div className="flex justify-center flex-wrap  bg-slate-200 rounded-xl">
+      <div className="flex justify-center flex-wrap md:mx-20 bg-slate-200 rounded-xl dark:bg-slate-600">
         {projects.map((project, index) => (
           <div
             key={index}
-            className="md:basis-1/2 shadow-lg p-10 rounded-xl my-2 flex flex-col justify-between text-center font-bold"
+            className="md:basis-1/2 md:px-10 shadow-lg p-10 rounded-xl my-2 flex flex-col justify-between text-center font-bold"
           >
             <img
               className="object-cover h-80 "
@@ -109,7 +124,12 @@ function App() {
               <a href={project.githubLink} target="_blank" rel="noreferrer">
                 <IoLogoGithub className="cursor-pointer hover:scale-125 transition-all ease-in-out text-[40px]" />
               </a>
-              <h2 className="pt-2">{project.name}</h2>
+
+              <div>
+              <h2 className="pt-2 text-2xl">{project.name}</h2>
+              <p className="text-small text-gray-600 dark:text-white">{project.text}</p>
+              </div>
+
               <a href={project.liveDemoLink} target="_blank" rel="noreferrer">
                 <IoDesktopOutline className="cursor-pointer hover:scale-125 transition-all ease-in-out text-[40px]" />
               </a>
@@ -126,10 +146,11 @@ function App() {
         <div className='flex flex-row justify-between gap-5 text-[30px]'>
         <a href="https://github.com/MWandall" target='_blank' rel="noreferrer"><IoLogoGithub className='  cursor-pointer hover:scale-125 transition-all ease-in-out'/></a>
         <a href="https://www.linkedin.com/in/mitchell-wandall-1501222a5/" target='_blank' rel="noreferrer"><IoLogoLinkedin className='  cursor-pointer hover:scale-125 transition-all ease-in-out'/></a>
-        <a href="https://discord.com/" target='_blank' rel="noreferrer"><IoLogoDiscord className=' cursor-pointer hover:scale-125 transition-all ease-in-out'/></a>
+        <p><IoCodeWorking  className=' cursor-pointer hover:scale-125 transition-all ease-in-out'/></p>
         </div>
         </footer>
     </main>
+    </div>
     </>
   )
 }
